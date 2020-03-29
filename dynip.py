f628c15857922052fb0e5871c5ea384f64f1b6bf
David Pietrzak <dtpietrzak@gmail.com>
2020-03-28 20:51:40 -0500

    Added dynip.conf usage to dynip.py. \n Now you need to set you Google Domains settings in dynip.conf as a space delineated, one lined string. I'll explain more in the readme later.

--- dynip.py
+++ dynip.py
@@ -2,19 +2,30 @@
 
 import requests, logging, datetime
 
-logging.basicConfig(filename="dynip.log", level=logging.INFO)
 
+# function to simplify printing to the log
+logging.basicConfig(filename="dynip.log", level=logging.INFO)
 def logEntry(string):
   logging.info(str(datetime.datetime.now())+" - "+string)
   print(string+"\n")
 
+# get the new ip from the lastest check
 with open("new_ip.txt", "r") as file:
   new_ip = file.read()
 file.close()
 
+# get the old ip from the last change
 with open("old_ip.txt", "r") as file:
   old_ip = file.read()
 file.close()
+
+# get the settings from the dynip.conf
+with open("dynip.conf", "r") as file:
+  settings = file.read()
+  set = settings.split()
+  username = set[0]
+  password = set[1]
+  domain = set[2]
 
 print("\nold: "+old_ip+"new: "+new_ip)
 
@@ -22,10 +33,6 @@ if old_ip == new_ip:
   logEntry('IP has not changed')
 else:
   print("IP has changed!\ncontacting domains.google.com...\n")
-
-  username = "" #put your google generated username here (between the quotes)
-  password = "" #put your google generated password here
-  domain = "" #put your Top Level Domain here (this needs to be the same as in the synthetic record on domains.google.com)
 
   url = "https://"+username+":"+password+"@domains.google.com/nic/update?hostname="+domain+"&myip="+new_ip
   head = {"User-Agent": "davidp-dynip/1.0"}
